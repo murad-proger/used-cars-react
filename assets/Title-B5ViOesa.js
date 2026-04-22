1042f7d82d348845ecdfe88c118b4b0ae35c1416
murad-proger <muradiusifov@gmail.com>
2026-04-22 23:36:46 +0400

    Updates

--- assets/Title-B5ViOesa.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-CMghyS6I.js";const p=({children:t,page:x,className:o=""})=>x?s.jsx("h1",{className:o,children:t}):s.jsx("h2",{className:o,children:t});export{p as T};
---
+++ assets/Title-C3rs7cyR.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BKG304lR.js";const p=({children:t,page:x,className:o=""})=>x?s.jsx("h1",{className:o,children:t}):s.jsx("h2",{className:o,children:t});export{p as T};
